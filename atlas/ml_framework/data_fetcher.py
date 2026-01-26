3fd4fc8308f9d001bcbfe6d59b3f37fb210b05b8
Artur Cordeiro Oudot Choi <acordeir@cern.ch>
2026-01-26 11:31:38 +0100

    Adding request_name to spec builder & structure simplification

--- atlas/ml_framework/data_fetcher.py
+++ atlas/ml_framework/data_fetcher.py
@@ -17,13 +17,17 @@ import os
 class RunConfig:
     tree_name: str
     dataset: str | list[str]
-    request_name: str = "ML_Training_Data"
+    request_name: str | list[str]
     output_folder: str = "./data/"
     files_per_sample: int = None
+    join_result_parquet: bool = True
 
     def __post_init__(self):
         if isinstance(self.dataset, str):
             self.dataset = [self.dataset]
+
+        if isinstance(self.request_name, str):
+            self.request_name = [self.request_name]
 
         if not self.output_folder.endswith("/"):
             self.output_folder += "/"
@@ -31,45 +35,44 @@ class RunConfig:
         if not os.path.exists(self.output_folder):
             os.makedirs(self.output_folder)
 
+        # If no request names provided, use dataset names
+        if self.request_name is None:
+            self.request_name = self.dataset
+
+        # Ensure request names and dataset have same length
+        if len(self.request_name) != len(self.dataset):
+            raise ValueError("Length of request_name must match length of dataset")
+
 
 # -------------------------
 # Query builder
 # -------------------------
-
-
 class ServiceXQuery:
-    def __init__(self, cuts=[None], selection=None):
-        self._cuts = cuts  # list of cut functions
-        self._select_fn = selection  # selection function
+    def __init__(self, cuts=[], selection=None, config=None):
+        self.cuts = cuts
+        self.selection = selection
+        self.config = config
+        self.func_adl_query = self.build()
 
-    def build(self, tree_name: str):
-        q = query.FuncADL_Uproot().FromTree(tree_name)
+    def build(self):
+        if self.config.tree_name is None:
+            raise RuntimeError("No tree name defined in configuration")
+        q = query.FuncADL_Uproot().FromTree(self.config.tree_name)
 
-        for cut in self._cuts:
+        for cut in self.cuts:
             q = q.Where(cut)
 
-        if self._select_fn is None:
+        if self.selection is None:
             raise RuntimeError("No Select() defined")
 
-        return q.Select(self._select_fn)
-
-
-# -------------------------
-# Executor (ServiceX bridge)
-# -------------------------
-
-
-class ServiceXExecutor:
-    def __init__(self, query_builder: ServiceXQuery, config: RunConfig):
-        self._query_builder = query_builder
-        self._cfg = config
+        return q.Select(self.selection)
 
     def write_to_parquet(self, files: dict, **kwargs):
         arrays = to_awk(files, **kwargs)
 
         for key, arr in arrays.items():
             logging.info("Key: %s, Entries: %d", key, len(arr))
-            file_path = self._cfg.output_folder + f"{key}.parquet"
+            file_path = self.config.output_folder + f"{key}.parquet"
 
             ak.to_parquet(
                 arr,
@@ -79,25 +82,27 @@ class ServiceXExecutor:
             )
 
     def deliver(self, **kwargs):
-        funcadl_query = self._query_builder.build(self._cfg.tree_name)
-
         queries = []
-        for sample in self._cfg.dataset:
+
+        for sample, name in zip(self.config.dataset, self.config.request_name):
             if not isinstance(sample, str):
                 raise ValueError("Input sample must be a string or list of strings")
 
             queries.append(
                 {
-                    "NFiles": self._cfg.files_per_sample,
-                    "Name": sample,
+                    "NFiles": self.config.files_per_sample,
+                    "Name": name,
                     "Dataset": ds_type_resolver(sample),
-                    "Query": funcadl_query,
+                    "Query": self.func_adl_query,
                 }
             )
 
         spec = {"General": {"Delivery": "LocalCache"}, "Sample": queries}
 
         files = deliver(spec, **kwargs)
-        self.write_to_parquet(files, **kwargs)
-        # Logging
-        logging.warning("Fetched data written to %s", self._cfg.output_folder)
+        if self.config.join_result_parquet:
+            self.write_to_parquet(files, **kwargs)
+            # Logging
+            logging.warning("Fetched data written to %s", self.config.output_folder)
+        else:
+            return files
